9d79bd1fe90ce5f2cd7b76f5bd9dc0758ce6abac
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-12-31 15:55:57 +0100

    Randomizzato le risposte

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,27 +5,33 @@ export default function Quiz(){
     const [ userAnswers, setUserAnswers ] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function handleSelectAnswer(selectedAnswer) {
-        setUserAnswers((prevUserAnswer) => {[...prevUserAnswer, selectedAnswer]});
+        setUserAnswers((prevUserAnswer) => {
+            return [...prevUserAnswer, selectedAnswer]
+        });
     }
 
     return (
-        <div id="question">
-            <h2>
-                {QUESTIONS[activeQuestionIndex].text}
-            </h2>
-            <ul id="answers">
-                {
-                    QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-                        <li key={answer} className="answer">
-                            <button onClick={() => handleSelectAnswer(answer)}>
-                                {answer}
-                            </button>
-                        </li>
-                    ))
-                }
-            </ul>
+        <div id="quiz">
+            <div id="question">
+                <h2>
+                    {QUESTIONS[activeQuestionIndex].text}
+                </h2>
+                <ul id="answers">
+                    {
+                        shuffledAnswers.map((answer) => (
+                            <li key={answer} className="answer">
+                                <button onClick={() => handleSelectAnswer(answer)}>
+                                    {answer}
+                                </button>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
         </div>
     );
 }
